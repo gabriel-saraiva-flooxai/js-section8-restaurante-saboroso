33396ff8fcf95a536509eb5518deceaab4d48eeb
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-09 17:25:02 -0300

    feat: making reservations with POST method to MySQL

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var conn = require('../inc/db')
 var express = require('express');
 var menus = require('../inc/menus')
+var reservations = require('../inc/reservations')
 var router = express.Router();
 
 /* GET home page. */
@@ -45,11 +46,35 @@ router.get('/menus', function(req, res, next){
 
 router.get('/reservations', function(req, res, next){
 
-  res.render('reservations', {
-    title: 'Reservas - Restaurante Saboroso!',
-    background: 'images/img_bg_2.jpg',
-    h1: 'Reserve uma Mesa!'
-  });
+  reservations.render(req, res);
+
+});
+
+router.post('/reservations', function(req, res, next){
+
+  if (!req.body.name) {
+    reservations.render(req, res, "Qual teu vulgo pae?");
+  } else if (!req.body.email) {
+    reservations.render(req, res, "Passa pra nois teu e-mail");
+  } else if (!req.body.people) {
+    reservations.render(req, res, "Vai tu e mais quem patrão?");
+  } else if (!req.body.date) {
+    reservations.render(req, res, "Pra que dia chefia?");
+  } else if (!req.body.time) {
+    reservations.render(req, res, "E que horas?");
+  } else {
+
+    reservations.save(req.body).then(results => {
+
+      req.body = {};
+
+      reservations.render(req, res, null, "Tá reservado painho!");
+
+    }).catch(err => {
+      reservations.render(req, res, err.message);
+    });
+
+  }
 
 });
 
