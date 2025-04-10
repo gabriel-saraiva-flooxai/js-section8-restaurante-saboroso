c96f4bf57bda742eefffb6ce089a886b9d948319
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-09 21:48:52 -0300

    feat: add login logic with user authentication

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var users = require('../inc/users');
 var router = express.Router();
 
 router.get("/", function(req, res, next){
@@ -9,7 +10,31 @@ router.get("/", function(req, res, next){
 
 router.get("/login", function(req, res, next){
 
-    res.render("admin/login");
+    users.render(req, res, null);
+
+});
+
+router.post("/login", function(req, res, next){
+
+    if (!req.body.email) {
+        users.render(req, res, "Preencha o campo e-mail.");
+    } else if (!req.body.password) {
+        users.render(req, res, "Preencha o campo senha.");
+    } else {
+
+        users.login(req.body.email, req.body.password).then(user => {
+
+            req.session.user = user;
+
+            res.redirect("/admin");
+
+        }).catch(err => {
+
+            users.render(req, res, err.message || err);
+
+        });
+
+    }
 
 });
 
