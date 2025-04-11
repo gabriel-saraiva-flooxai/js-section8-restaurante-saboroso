38b8f5821cca97bb7551de2415caef252dc8afe3
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-11 17:09:54 -0300

    feat: read and delete admin's contacts

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ var users = require('../inc/users');
 var admin = require('../inc/admin');
 var menus = require('../inc/menus');
 var reservations = require('../inc/reservations');
+var contacts = require('../inc/contacts');
 var moment = require('moment');
 var router = express.Router();
 
@@ -80,7 +81,23 @@ router.post("/login", function(req, res, next){
 
 router.get("/contacts", function(req, res, next){
 
-    res.render("admin/contacts", admin.getParams(req));
+    contacts.getContacts().then(data => {
+
+        res.render("admin/contacts", admin.getParams(req, {
+            data
+        }));
+
+    });
+
+});
+
+router.delete("/contacts/:id", function(req, res, next){
+
+    contacts.delete(req.params.id).then(results => {
+        res.send(results);
+    }).catch(err => {
+        res.send(err);
+    });
 
 });
 
